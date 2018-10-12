360fb0c30a02515db58d5e88cc2b144d42fb2175
Thiago Santos <thia.mdossantos@gmail.com>
2018-10-12 12:04:59 -0300

    feat: add react-router setup

--- src/app.js
+++ src/app.js
@@ -2,10 +2,33 @@ import React from 'react'
 import Button from '@material-ui/core/Button'
 import SaveIcon from '@material-ui/icons/Save'
 
+import { BrowserRouter as Router, Route, Link,Switch } from "react-router-dom"
+
+const Home = () => (
+  <div>
+<Link to="/about"><Button variant="contained" color="primary">
+  about<SaveIcon/>
+</Button></Link>
+</div>
+);
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+<Link to="/">home</Link>
+
+  </div>
+);
+
+
 const App = () => (
-  <div><Button variant="contained" color="primary">
-  Hello World<SaveIcon/>
-</Button></div>
+  <Router>
+    <Switch>
+
+  <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+    </Switch>
+    </Router>
 )
 
 export default App
