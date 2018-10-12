9baaf434279d78b3029f8529556cbc35c88213f9
Thiago Santos <thia.mdossantos@gmail.com>
2018-10-12 13:07:27 -0300

    feat: add apollo client setup

--- src/app.js
+++ src/app.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {default as ApolloClient} from 'apollo-boost'
+import {ApolloProvider} from 'react-apollo'
 import {Home, About} from './routes'
 
+const client = new ApolloClient({
+  uri: 'https://graphql-pokemon.now.sh'
+})
+
 const App = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-    </Switch>
-  </Router>
+  <ApolloProvider client={client}>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </Switch>
+    </Router>
+  </ApolloProvider>
 )
 
 export default App
--- src/screens/home.js
+++ src/screens/home.js
@@ -1,11 +1,42 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
 import SaveIcon from '@material-ui/icons/Save'
-
 import {Link} from 'react-router-dom'
+import {Query} from 'react-apollo'
+import gql from 'graphql-tag'
+
+const PIKACHU_QUERY = gql`
+  {
+    pokemon(name: "Pikachu") {
+      id
+      number
+      name
+    }
+  }
+`
+
+const Pikachu = () => (
+  <Query query={PIKACHU_QUERY}>
+    {({loading, error, data}) => {
+      if (loading) return <p>Loading...</p>
+      if (error) return <p>Error :(</p>
+
+      const {id, number, name} = data.pokemon
+
+      return (
+        <div>
+          <p>ID: {id}</p>
+          <p>Number: {number}</p>
+          <p>Name: {name}</p>
+        </div>
+      )
+    }}
+  </Query>
+)
 
 const Home = () => (
   <div>
+    <Pikachu />
     <Link to="/about">
       <Button variant="contained" color="primary">
         about
